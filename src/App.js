aaee3ec3cee8f7d196227b4034e556b1a6b22134
Gokul-nath-K <gokulnathk003@gmail.com>
2023-02-03 23:11:14 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './Page/HomePage';
+import Player from './components/Player';
+import AppTopBar from './components/AppTopBar';
+import SideBar from './components/sideBar';
+
+import { userContext, songContext, songs } from './Data/userContext';
+import { SongData } from './Data/Songs';
+import { useState } from 'react';
 
 function App() {
+
+  const [isplaying, setisplaying] = useState(false);
+  const [currentsong, setcurrentsong] = useState(SongData[0]);
+  const [song, setsong] = useState(SongData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+      <div className="app-top-bar">
+        <AppTopBar />
+      </div>
+      <div className="side-nav-bar">
+        <SideBar />
+      </div>
+      <div className="content-container">
+          <userContext.Provider value={{ isplaying, setisplaying }} >
+            <songContext.Provider value={{ currentsong, setcurrentsong }} >
+              <songs.Provider value={{ song, setsong }} >
+              <HomePage />
+            </songs.Provider>
+          </songContext.Provider>
+        </userContext.Provider>
+      </div>
+      <div className="music-control-bar">
+        <userContext.Provider value={{ isplaying, setisplaying }} >
+          <songContext.Provider value={{ currentsong, setcurrentsong }} >
+            <songs.Provider value={{ song, setsong }} >
+              <Player />
+            </songs.Provider>
+          </songContext.Provider>
+        </userContext.Provider>
+      </div>
+    </>
     </div>
   );
 }
